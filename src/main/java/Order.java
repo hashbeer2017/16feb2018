f32538b3bec8fba9e36e1bf8b0ad2092b4716771
domevit <domenico-vitarella@hotmail.it>
2018-02-16 20:34:34 +0100

    order update

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
  */
 public class Order extends GridObject{
     private int id;
-    private HashMap<Product, Integer> products;
+    private HashMap<Integer, Integer> products;
 
-    public Order(int x, int y, int id, HashMap<Product, Integer> products) {
+    public Order(int x, int y, int id, HashMap<Integer, Integer> products) {
         super.coordinate = new Coordinate(x,y);
         this.id = id;
         this.products = products;
@@ -21,11 +21,11 @@ public class Order extends GridObject{
         return id;
     }
 
-    public void setProducts(HashMap<Product, Integer> products) {
+    public void setProducts(HashMap<Integer, Integer> products) {
         this.products = products;
     }
 
-    public HashMap<Product, Integer> getProducts() {
+    public HashMap<Integer, Integer> getProducts() {
         return products;
     }
 
